c32548562a382721ef6edaa20f5a84ec7e54cdd2
CarlebeC <carlebe.cha@gmail.com>
2026-01-20 18:11:17 -0500

    statments

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 print("Carlebe Charitable")
+#Using the hashtag/pound symbol is a great way to write comments.
+print("Hello, how are you today?")
+
+name = "Scooby" #varibles unlike other languages don't have a type.
+
+#int, double, float, char, string, boolean, ==
+age = 23
+temperature = 98.7
+
+#types of printing
+print("I am {} years old, and the temperature is {} degrees".format(age, temperature))
+
+#input
+friendName = input("What is your name?\n ")
+print(friendName)
+
+fage = int(input("What is your favorite age?\n "))
+print(fage)
+
+total = 52 + 78
+print(total)
+
+agesTotal = fage + age
+print(agesTotal)
+
+modulusTotal = 5 % 2
+print(modulusTotal)
