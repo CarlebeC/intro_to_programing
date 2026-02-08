13c31f986dc161d2a411b7230c2450378fc538a2
CarlebeC <carlebe.cha@gmail.com>
2026-02-07 23:28:41 -0500

    Create Variables_Assignment2.py
    Updated variables assignment

---
+++ Variables_Assignment2.py
@@ -0,0 +1,38 @@
+#That we want to multiply two numbers
+numMulti = int(input("Enter a number: "))
+numMulti2 = int(input("Enter a number: "))
+answer = numMulti * numMulti2
+print("The answer to your query is", answer)
+
+# That we want to divide two numbers
+numDiv = int(input("Enter a number: "))
+numDiv2 = int(input("Enter a number: "))
+answerDiv = numDiv / numDiv2
+print("The answer to your query is {:.8f}".format(answerDiv))
+
+#That we divide without remainder two numbers
+numbNoRemain = int(input("Enter a number: "))
+numbNoRemain2 = int(input("Enter a number: "))
+answerRemain = numbNoRemain // numbNoRemain2
+print("The answer to your query is", answerRemain)
+
+#That we want the remainder of two numbers
+numDiv = int(input("Enter a number: "))
+numDiv2 = int(input("Enter a number: "))
+answerDiv = numDiv % numDiv2
+print("The answer to your query is", answerDiv)
+
+#That we want the sum of 2 numbers
+numSum = int(input("Enter a number: "))
+numSum2 = int(input("Enter a number: "))
+answersum = numSum + numSum2
+print("The answer to your query is", answersum)
+
+#That we want the difference of the two mumbers
+numSub = int(input("Enter a number: "))
+numSub2 = int(input("Enter a number: "))
+answersub = numSub - numSub2
+print("The answer to your query is", answersub)
+
+# Print name (last line)
+print("Carlebe Charitable")
